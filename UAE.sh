9e59e8a130e02df3f7f1eec59ad5243018253bbc
A1brt <albert.amadanyan@cloudchipr.com>
2022-06-22 17:59:06 +0400

    more of a compressor then archiver

--- UAE.sh
+++ UAE.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-if [[ $1 == "-unzip" ]]; then
-   echo "unzip"
+LOCATOIN=`which tar`
+if [[ $? -ne 0 ]]; then
+	echo "tar not installed on this system, please install tar before running this command"
+	exit 1
+fi
+
+# decomressing a tar archive if option -d is selected
+# input format: bash UAE.sh -d <file> for current working directory or bash UAE.sh -d <file> <new-location> for decompressing into another directory
+
+if [[ $1 == "-d" ]]; then
    if [[ $3 ]]; then
       tar -xvf $2 -C $3
    else
-      echo hello
       tar -xvf $2
    fi
-elif [[ $1 == "-zip" ]]; then
-   echo "zip"
-   case $2 in
-        "-gzip")
-          tar -czvf "${3}.gz" $4
-          ;;
-        "-bzip2")    
-          tar -cjvf "${3}.bz2" $4
-          ;;
-        "-xz")    
-          tar -cJvf "${3}.xz" $4
-          ;; 
-    esac
-else 
-   exit 1
+   exit 0
 fi
 
+# compressing a tar archive if option -d was not selected
+case $1 in
+       "-gzip")
+          tar -czvf "${2}.gz" $3
+          ;;
+       "-bzip2")    
+          tar -cjvf "${2}.bz2" $3
+          ;;
+       "-xz")    
+          tar -cJvf "${2}.xz" $3
+          ;; 
+esac
+exit 0

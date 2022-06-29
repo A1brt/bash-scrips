263442cbaadbbf7cbd056ddcdf0be75d11873d38
A1brt <albert.amadanyan@cloudchipr.com>
2022-06-29 17:18:04 +0400

    i guess

--- UAE.sh
+++ UAE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOCATOIN=`which tar`
+LOCATOIN=$(which tar)
 if [[ $? -ne 0 ]]; then
 	echo "tar not installed on this system, please install tar before running this command"
 	exit 1
@@ -9,6 +9,8 @@ fi
 # decomressing a tar archive if option -d is selected
 # input format: bash UAE.sh -d <file> for current working directory or bash UAE.sh -d <file> <new-location> for decompressing into another directory
 
+
+PARSED_ARGUMENTS=$(getopt -a -n UAE -o dg:b:x: -l decompress,gzip:,bzip2:,xz: "$@")
 if [[ $1 == "-d" ]]; then
    if [[ $3 ]]; then
       tar -xvf $2 -C $3
@@ -19,15 +21,15 @@ if [[ $1 == "-d" ]]; then
 fi
 
 # compressing a tar archive if option -d was not selected
-# imput format bash UAE.sh -gzip <name>.tar <path-to-tar>
+# imput format bash UAE.sh -gzip <name> <path-to-tar>
 case $1 in
-       "-gzip")
+       -g|--gzip)
           tar -czvf "${2}.tar.gz" $3
           ;;
-       "-bzip2")    
+       -b|--bzip2)    
           tar -cjvf "${2}.tar.bz2" $3
           ;;
-       "-xz")    
+       -x|--xz)    
           tar -cJvf "${2}.tar.xz" $3
           ;; 
 esac
